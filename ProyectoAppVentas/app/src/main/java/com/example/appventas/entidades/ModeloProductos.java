0bd1cd9b2413e68fac907692a8ca95b2312f0a31
jesussan2811 <60827768+jesussan2811@users.noreply.github.com>
2023-09-14 14:02:24 -0700

    commit 4 modelos y adaptadores

--- ProyectoAppVentas/app/src/main/java/com/example/appventas/entidades/ModeloProductos.java
+++ ProyectoAppVentas/app/src/main/java/com/example/appventas/entidades/ModeloProductos.java
@@ -1,4 +1,54 @@
 package com.example.appventas.entidades;
 
 public class ModeloProductos {
+    private int codigo;
+    private String prodNombre;
+    private String descripcion;
+    private String marca;
+    private int cantidad;
+    private double precio;
+
+    public ModeloProductos() {}
+
+    public ModeloProductos(int codigo, String prodNombre, String descripcion, String marca, int cantidad, double precio) {
+        this.codigo = codigo;
+        this.prodNombre = prodNombre;
+        this.descripcion = descripcion;
+        this.marca = marca;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    public ModeloProductos(String prodNombre, String descripcion, String marca, int cantidad, double precio) {
+        this.prodNombre = prodNombre;
+        this.descripcion = descripcion;
+        this.marca = marca;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getProdNombre() {
+        return prodNombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
 }
